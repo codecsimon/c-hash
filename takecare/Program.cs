7720bc40bb7da3da618c6c84023cd6093a5f85a5
Simon Ngugi <snmwanjiru@gmail.com>
2023-07-04 05:03:02 -0700

    slowdown

---
+++ takecare/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Tear the road");
+Console.WriteLine("Enter Name?");
+string? look = Console.ReadLine();
+Console.WriteLine("Name is :"+look);
