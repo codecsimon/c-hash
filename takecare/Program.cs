05ab0358ad83d6aff3ff94d00d81db171b833a8c
Simon Ngugi <snmwanjiru@gmail.com>
2023-07-04 06:24:19 -0700

    credentials loko

--- takecare/Program.cs
+++ takecare/Program.cs
@@ -1,18 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿internal class Program
+{
+    public static List<havana> babu =new List<havana>();
+    private static void Main(string[] args)
+    {
+        
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Tear the road");
 
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Tear the road");
-
-Console.WriteLine("Name is :"+look);
-havana xol =new havana();
-Console.WriteLine("Enter Name?");
-xol.Name= Console.ReadLine();
-Console.WriteLine("Enter Age");
-xol.Age = Convert.Tobase32(Console.ReadLine());
+        Console.WriteLine("Name is :" );
+        havana xol = new havana();
+        Console.WriteLine("Enter Name?");
+        xol.Name = Console.ReadLine();
+        Console.WriteLine("Enter Age");
+        xol.Age = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter Email");
+        xol.Email = Console.ReadLine();
+        havana xpo=new havana();
+      babu.Add(new havana{Age=20,Name="Waithera",Email="wam@gmail.com"});
+      babu.Add(new havana{Age=10,Name="Wambui",Email="mbui@gmail.com"});
+      babu.Add(new havana{Age=40,Name="Wamere",Email="mere@gmail.com"});
+      babu.Add(new havana{Age=90,Name="Drek",Email="drek@gmail.com"});
+       
+        foreach(var drek in babu)
+        {
+            Console.WriteLine(drek.Age+"|--|"+drek.Email+"|--|"+drek.Name);
+        }
+    }
+}
 
 public class havana
     {
-        //This is a model we will use later
+        //This is a model uwe will use later
         public int Age { get; set; }
         public string? Name{ get; set; }    
         public string? Email { get; set; }
