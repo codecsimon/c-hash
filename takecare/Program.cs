4152d626d317a3cfa570522005d7d5c180d94d52
Simon Ngugi <snmwanjiru@gmail.com>
2023-07-04 05:33:12 -0700

    We tulipitia hapa jana

--- takecare/Program.cs
+++ takecare/Program.cs
@@ -1,6 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Tear the road");
-Console.WriteLine("Enter Name?");
-string? look = Console.ReadLine();
+
 Console.WriteLine("Name is :"+look);
+havana xol =new havana();
+Console.WriteLine("Enter Name?");
+xol.Name= Console.ReadLine();
+Console.WriteLine("Enter Age");
+xol.Age = Convert.Tobase32(Console.ReadLine());
+
+public class havana
+    {
+        //This is a model we will use later
+        public int Age { get; set; }
+        public string? Name{ get; set; }    
+        public string? Email { get; set; }
+        
+        
+        }
